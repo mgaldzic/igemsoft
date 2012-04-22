baf17b358e887e211d5cbbe4d96c1876c3602510
Michal Galdzicki <mgaldzic@uw.edu>
2012-04-21 19:23:12 -0700

    initial commit

---
+++ 20120421igem_soft_links.js
@@ -0,0 +1,61 @@
+{
+	"items" :      [
+		{
+			"url" :         "http:\/\/2008.igem.org\/Team:Calgary_Software\/Notebook and http:\/\/2008.igem.org\/Team:Calgary_Software\/Project",
+			"category" :    "Circuit design and simulation",
+			"uri" :         "http:\/\/igem.synbioreview.com\/20120421igem_soft_links.txt#EvoGEM",
+			"team" :        "Calgary_Software",
+			"update" :      "2008",
+			"status" :      "not active",
+			"contact" :     "igem@ucalgary.ca",
+			"type" :        "Item",
+			"year" :        "2008",
+			"label" :       "EvoGEM",
+			"description" : "\"A software program that uses the registry of genetic parts provided by the iGEM competition. In this design, we used evolutionary and genetic strategies, which are useful modeling methods, especially when coupled with agent-based designs.\""
+		},
+		{
+			"url" :         "http:\/\/2008.igem.org\/Team:Bologna\/Software",
+			"category" :    "Repository",
+			"uri" :         "http:\/\/igem.synbioreview.com\/20120421igem_soft_links.txt#ARQ",
+			"team" :        "Bologna",
+			"update" :      "2008",
+			"status" :      "active",
+			"contact" :     "igem@ucalgary.ca",
+			"type" :        "Item",
+			"year" :        "2008",
+			"label" :       "ARQ",
+			"description" : "a Java tool to query the Registry"
+		}
+	],
+	"types" :      {
+		"Item" : {
+			"uri" : "http:\/\/igem.synbioreview.com\/20120421igem_soft_links.txt#Item"
+		}
+	},
+	"properties" : {
+		"contact" :     {
+			"uri" : "http:\/\/igem.synbioreview.com\/20120421igem_soft_links.txt#contact"
+		},
+		"status" :      {
+			"uri" : "http:\/\/igem.synbioreview.com\/20120421igem_soft_links.txt#status"
+		},
+		"team" :        {
+			"uri" : "http:\/\/igem.synbioreview.com\/20120421igem_soft_links.txt#team"
+		},
+		"url" :         {
+			"uri" : "http:\/\/igem.synbioreview.com\/20120421igem_soft_links.txt#url"
+		},
+		"year" :        {
+			"uri" : "http:\/\/igem.synbioreview.com\/20120421igem_soft_links.txt#year"
+		},
+		"category" :    {
+			"uri" : "http:\/\/igem.synbioreview.com\/20120421igem_soft_links.txt#category"
+		},
+		"description" : {
+			"uri" : "http:\/\/igem.synbioreview.com\/20120421igem_soft_links.txt#description"
+		},
+		"update" :      {
+			"uri" : "http:\/\/igem.synbioreview.com\/20120421igem_soft_links.txt#update"
+		}
+	}
+}
